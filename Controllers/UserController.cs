52583eb2ca1ecc06b55bab20737b0830d009c212
conrad222 <76107590+conrad222@users.noreply.github.com>
2023-06-23 19:50:03 +0200

    mas

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,15 +1,62 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApuestasWebCaballos.Models.DataAcces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApuestasWebCaballos.Controllers
 {
     public class UserController : Controller
     {
+        private readonly CarreraswebContext db = new CarreraswebContext();
+
         // GET: UserController
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuario);
+                db.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+
+            return View(usuario);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string correo, string clave)
+        {
+            Usuario usuario = db.Usuarios.FirstOrDefault(u => u.Email == correo && u.Password == clave);
+
+            if (usuario != null)
+            {
+                Session["usuario_id"] = usuario.Id;
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Correo o contraseña incorrectos");
+
+            return View();
+        }
+
 
         // GET: UserController/Details/5
         public ActionResult Details(int id)
